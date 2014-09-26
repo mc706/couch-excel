9b524cda376fb4ed2cf389b646934a79f91aa3a1
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-09-26 09:15:29 -0400

    added number option to dynamic fields

--- _attachments/app/directives/dynamicField.js
+++ _attachments/app/directives/dynamicField.js
@@ -2,19 +2,7 @@ app.directive('dynamicField', function ($compile) {
     "use strict";
     return {
         restrict: "E",
-        templateUrl: 'app/directives/dynamic-field.html'/*,
-        link: function(scope) {
-            //scope.localModel = scope.serverNameModel();
-
-            // bind from ei-server -> parent
-            scope.$watch('localModel', function(value) {
-                scope.serverNameModel(value);
-            });
-
-            // bind from parent -> ei-server
-            scope.$watch(function() {return scope.serverNameModel()}, function(value) {
-                scope.localModel = value;
-                });
-        }*/
+        scope: false,
+        templateUrl: 'app/directives/dynamic-field.html'
     };
 });
